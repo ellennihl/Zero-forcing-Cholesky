9f6edf54b812a05573d22c17faec359ca60ba0e0
Ellen <50663696+ellennihl@users.noreply.github.com>
2023-04-11 12:59:06 +0200

    cholesky test

--- ellentest.cu
+++ ellentest.cu
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <cuda.h>
 #include <stdlib.h>
+#include <cuComplex.h>
 
 __global__ void hermitian_transpose(const float2* input_h, float2* output_hh, int N, int K) { //const because we do not want to modify the input matrix!!!
 
@@ -62,19 +63,46 @@ __global__ void complex_matrix_mult(const float2* A, const float2* B, float2* C,
 	}
 }
 
-__global__ void cholesky(float2 *gramian, int size, float2 *L, float2 *L_T){
-	int row = blockIdx.y * blockDim.y + threadIdx.y;
-    int col = blockIdx.x * blockDim.x + threadIdx.x;
+//column by column
+__global__ void cholesky(float2 *A, int column, const int size, float2 *L_T){
+	int col = threadIdx.x + blockDim.x * blockIdx.x; //find what col and row this thread is responsible for
+	int row = threadIdx.y + blockDim.y * blockIdx.y;
 	
-	//calc index
-	//do cholesky col
-	//wait for prev cholesky(event)
-	//when ready, do mat inv
-	//matmul
-	//matmul
-	//or only cholesky here
+	int idx = column * size + row; //find index
+	int transpose_idx = row * size + column;
+	int diagonal = (column * size) + column;
+	//float2 diagonal = A[(col * N) + (row + (col - row))];//gives me the diagonal element, stupid maybe?????
 	
+	if(idx == diagonal){//if diagonal element
+		A[idx] = make_float2(1.0,1.0);//sqrt(A[idx]);
+		L_T[transpose_idx] = A[idx];
+	}
+	else{
+		A[idx] = make_float2(2.0,2.0);//A[idx]/A[diagonal];
+		L_T[transpose_idx] = A[idx];
+	}
+	printf("chol: (%d,%d) idx: %d column: %d diagonal: %d \n", row,col,idx,column, diagonal);
 }
+
+/*__global__ void cholesky2(float2 *A, float2 diagonal, int size, float2 *L, float2 *L_T){
+	int row = threadIdx.x + blockDim.x * blockIdx.x; //find what col and row this thread is responsible for
+	int col = threadIdx.y + blockDim.y * blockIdx.y;
+	
+	int idx = col * size + row; //find index
+	float2 diagonal = A[(col * N) + (row + (col - row))];//gives me the diagonal element, very unnecessary maybe
+	
+	if(row == col){//if diagonal element
+		A[idx] = sqrt(A[idx]);
+	}
+	else{
+		A[idx+1] = A[idx+1]/A[idx];
+}
+}*/
+
+/*__global__ void cholesky3(float2 *A, int column, int size){
+	
+	
+}*/
 
 int main(int argc, char *argv[])  {
 /*
@@ -98,11 +126,11 @@ int main(int argc, char *argv[])  {
 		printf("(%f + %fi)\n", h[i].x, h[i].y);
 	}
 	
-	float2 *mat_h,*mat_hh,*mat_hhh, *vec_y, *vec_hy; //float2 z = {1.0f, 2.0f}; // z = 1.0 + 2.0i
-	int mat1_size;					// number of bytes in arrays
-	int mat2_size;
-	int vec1_size;
-	int vec2_size;
+	float2 *mat_h,*mat_hh,*mat_hhh, *vec_y, *vec_hy, *mat_l, *mat_lh; //float2 z = {1.0f, 2.0f}; // z = 1.0 + 2.0i
+	int matKN_size;					// number of bytes in arrays
+	int matKK_size;
+	int vecN_size;
+	int vecK_size;
 	
 	cudaEvent_t start, stop;     		// using cuda events to measure time
 	float elapsed_time_ms;       		// which is applicable for asynchronous code also
@@ -122,21 +150,23 @@ int main(int argc, char *argv[])  {
 	dim3 Grid(Grid_Dim_x, Grid_Dim_y);		//Grid structure
 	dim3 Block(Block_Dim_x,Block_Dim_y);	//Block structure, threads/block limited by specific device
 
-	mat1_size = K * N * sizeof(float2); //Hh and H are K*N and N*K
-	mat2_size = K * K * sizeof(float2);	//gramian is K*K
-	vec1_size = N * sizeof(float2);		//vec is K*1
-	vec2_size = K * sizeof(float2);
+	matKN_size = K * N * sizeof(float2); //Hh and H are K*N and N*K
+	matKK_size = K * K * sizeof(float2);	//gramian is K*K
+	vecN_size = N * sizeof(float2);		//vec is K*1
+	vecK_size = K * sizeof(float2);
 
 /* ------------- COMPUTATION DONE ON GPU ----------------------------*/
 
-	cudaMalloc((void**)&mat_h, mat1_size);		// allocate memory on device
-	cudaMalloc((void**)&mat_hh, mat1_size);
-	cudaMalloc((void**)&mat_hhh, mat2_size);
-	cudaMalloc((void**)&vec_y, vec1_size); //size of y vector is Nx1
-	cudaMalloc((void**)&vec_hy, vec2_size); //size of Hy vector is Nx1
+	cudaMalloc((void**)&mat_h, matKN_size);		// allocate memory on device
+	cudaMalloc((void**)&mat_hh, matKN_size);
+	cudaMalloc((void**)&mat_hhh, matKK_size);
+	cudaMalloc((void**)&vec_y, vecN_size); //size of y vector is Nx1
+	cudaMalloc((void**)&vec_hy, vecK_size); //size of Hy vector is Nx1
+	cudaMalloc((void**)&mat_l, matKK_size);
+	cudaMalloc((void**)&mat_lh, matKK_size);
 
-	cudaMemcpy(mat_h, h, mat1_size ,cudaMemcpyHostToDevice); //put h in device
-	cudaMemcpy(vec_y, y, vec1_size ,cudaMemcpyHostToDevice); //put y in device
+	cudaMemcpy(mat_h, h, matKN_size ,cudaMemcpyHostToDevice); //put h in device
+	cudaMemcpy(vec_y, y, vecN_size ,cudaMemcpyHostToDevice); //put y in device
 
 //--------------------------TRANSPOSE-Hh---------------------------------
 	cudaEventCreate(&start);     		// instrument code to measure start time
@@ -150,7 +180,7 @@ int main(int argc, char *argv[])  {
 	hermitian_transpose<<<Grid,Block>>>(mat_h,mat_hh,N,K); //calc hermitian Hh
 
 	float2 output[N*K];//just to print, device has mat_hh, host does not need it?
-	cudaMemcpy(output, mat_hh, mat1_size, cudaMemcpyDeviceToHost);
+	cudaMemcpy(output, mat_hh, matKN_size, cudaMemcpyDeviceToHost);
 
 	cudaEventRecord(stop, 0);     	// instrument code to measure end time
 	cudaEventSynchronize(stop);
@@ -165,14 +195,11 @@ int main(int argc, char *argv[])  {
 //-------------------------MATMUL-HhH--------------------------------------
 	//a is H, output is Hh
 	//this is HhH
-	//KNK
-	//NKK
-	//KKN
 	complex_matrix_mult<<<Grid,Block>>>(mat_h, mat_hh, mat_hhh, K,N,K); //why no work
 	//complex_matrix_mult<<<Grid,Block>>>(mat_h, mat_hh, mat_hhh, K,N,K);//A, B, C, res_row, a_row_b_col, res_col
 
 	float2 gramian[K*K];
-	cudaMemcpy(gramian, mat_hhh, mat2_size, cudaMemcpyDeviceToHost);
+	cudaMemcpy(gramian, mat_hhh, matKK_size, cudaMemcpyDeviceToHost);
 
 	printf("gramian\n");
 	for (int i = 0; i < K*K; i++) {
@@ -186,11 +213,41 @@ int main(int argc, char *argv[])  {
 	//complex_matrix_mult<<<Grid,Block>>>(vec_y, mat_hh, vec_hy, 1,N,K);//WHY IS IT FLIPPED
 	
 	float2 hy[K];
-	cudaMemcpy(hy, vec_hy, vec2_size, cudaMemcpyDeviceToHost);
+	cudaMemcpy(hy, vec_hy, vecK_size, cudaMemcpyDeviceToHost);
 	
 	printf("Hy\n");
 	for (int i = 0; i < K; i++) {
 		printf("(%f + %fi)\n", hy[i].x, hy[i].y);
+	}
+
+//--------------------------Cholesky--------------------------------------
+//this should be in a thread later
+	Grid_Dim_x = 1;
+	Grid_Dim_y = 1;
+	Block_Dim_x = 1;
+	Block_Dim_y = 2;
+	
+    for (int col = 0; col < K; col++) {
+		
+		dim3 Grid2(Grid_Dim_x, Grid_Dim_y);		//Grid structure
+		dim3 Block2(Block_Dim_x,Block_Dim_y--);	//Block structure, threads/block limited by specific device
+		
+        cholesky<<<Grid2, Block2>>>(mat_hhh, col, K, mat_lh);
+		
+
+        if (col > 0) {
+            cudaDeviceSynchronize();
+        }
+    }
+	
+	float2 l[K*K];
+	
+	cudaMemcpy(l, mat_hhh, matKK_size, cudaMemcpyDeviceToHost);
+	
+	printf("lite chol\n");
+	for(int i = 0; i < K*K; i++){
+		printf("%f + %f\n",l[i].x, l[i].y);
+		
 	}
 
 /* --------------  clean up  ---------------------------------------*/
@@ -199,6 +256,8 @@ int main(int argc, char *argv[])  {
 	cudaFree(mat_hhh);
 	cudaFree(vec_y);
 	cudaFree(vec_hy);
+	cudaFree(mat_l);
+	cudaFree(mat_lh);
 
 	cudaEventDestroy(start);
 	cudaEventDestroy(stop);
