2577c464782da04754b10496ef82a1c44c63533e
Ellen <50663696+ellennihl@users.noreply.github.com>
2023-04-26 15:39:00 +0200

    serial works to cholesky, only inv left
    made a version that works with doubles instead as well

--- serial/ZF_serial_double.c
+++ serial/ZF_serial_double.c
@@ -7,17 +7,17 @@
 #include <unistd.h>
 #include <complex.h>
 
-typedef struct float2{
-	float x; //real
-	float y; //imaginary
-} float2;
+typedef struct double2{
+	double x; //real
+	double y; //imaginary
+} double2;
 
 /**
 	Takes a csv containing a matrix and returns an array column major
 */
-float2 *read_matrix_from_csv(char filename[], int num_rows, int num_cols) {
+double2 *read_matrix_from_csv(char filename[], int num_rows, int num_cols) {
 	// Allocate memory for the matrix
-	float2 *matrix = (float2 *) malloc(num_rows * num_cols * sizeof(float2));
+	double2 *matrix = (double2 *) malloc(num_rows * num_cols * sizeof(double2));
 	int real = 1;
 
 	char tempchar[20];
@@ -67,7 +67,7 @@ float2 *read_matrix_from_csv(char filename[], int num_rows, int num_cols) {
 }
 
 
-void hermitian_transpose(const float2* input_h, float2* output_hh, int K, int N) { //const because we do not want to modify the input matrix
+void hermitian_transpose(const double2* input_h, double2* output_hh, int K, int N) { //const because we do not want to modify the input matrix
 	
 	for(int row = 0; row<N; row++){
 		for(int col = 0; col<K; col++){
@@ -82,19 +82,19 @@ void hermitian_transpose(const float2* input_h, float2* output_hh, int K, int N)
 	}
 }
 
-void complex_matrix_mult(const float2* A, const float2* B, float2* C, const int res_row, const int a_col_b_row, const int res_col) {
+void complex_matrix_mult(const double2* A, const double2* B, double2* C, const int res_row, const int a_col_b_row, const int res_col) {
 
 	for(int row = 0; row<res_row; row++){
 		for(int col = 0; col<res_col; col++){
-        float2 sum = {.x = 0.0f, .y = 0.0f};
+        double2 sum = {.x = 0.0f, .y = 0.0f};
 
         for (int k = 0; k < a_col_b_row; k++) {
 			//printf("(%d,%d) a: %d   b: %d\n",row,col,row * a_col_b_row + k, k * res_col + col);
 
-            float2 a = A[k * res_row + row]; //column-major!!!!!!
-            float2 b = B[col * a_col_b_row + k];
-            float real_part = a.x * b.x - a.y * b.y;
-            float imag_part = a.x * b.y + a.y * b.x;
+            double2 a = A[k * res_row + row]; //column-major!!!!!!
+            double2 b = B[col * a_col_b_row + k];
+            double real_part = a.x * b.x - a.y * b.y;
+            double imag_part = a.x * b.y + a.y * b.x;
             sum.x += real_part;
             sum.y += imag_part;
 			}
@@ -104,19 +104,19 @@ void complex_matrix_mult(const float2* A, const float2* B, float2* C, const int 
 	}
 }
 
-void L_triangular_complex_matrix_mult(const float2* A, const float2* B, float2* C, const int res_row, const int a_col_b_row, const int res_col) {
+void L_triangular_complex_matrix_mult(const double2* A, const double2* B, double2* C, const int res_row, const int a_col_b_row, const int res_col) {
 
 	for(int row = 0; row<res_row; row++){
 		for(int col = 0; col<res_col; col++){
-			float2 sum = {.x = 0.0f, .y = 0.0f};
+			double2 sum = {.x = 0.0f, .y = 0.0f};
 			if(row>=col){
 				for (int k = 0; k < a_col_b_row; k++) {
 					//printf("(%d,%d) a: %d   b: %d\n",row,col,row * a_col_b_row + k, k * res_col + col);
 
-					float2 a = A[k * res_row + row]; //column-major!!!!!!
-					float2 b = B[col * a_col_b_row + k];
-					float real_part = a.x * b.x - a.y * b.y;
-					float imag_part = a.x * b.y + a.y * b.x;
+					double2 a = A[k * res_row + row]; //column-major!!!!!!
+					double2 b = B[col * a_col_b_row + k];
+					double real_part = a.x * b.x - a.y * b.y;
+					double imag_part = a.x * b.y + a.y * b.x;
 					sum.x += real_part;
 					sum.y += imag_part;
 					}
@@ -127,23 +127,23 @@ void L_triangular_complex_matrix_mult(const float2* A, const float2* B, float2* 
 	}
 }
 
-/*float2 csqrt(float2 z){
-	float complex z = z.x + z.y*I;
-	float r = cabsf(z);
-    float theta = atan2(z.y,z.x);
-    float2 sqrt_z = {.x = sqrtf(r) * cosf(theta / 2.0f), .y = sqrtf(r) * sinf(theta / 2.0f)}//;make_cuFloatComplex(sqrtf(r) * cosf(theta / 2.0f),
+/*double2 csqrt(double2 z){
+	double complex z = z.x + z.y*I;
+	double r = cabsf(z);
+    double theta = atan2(z.y,z.x);
+    double2 sqrt_z = {.x = sqrtf(r) * cosf(theta / 2.0f), .y = sqrtf(r) * sinf(theta / 2.0f)}//;make_cudoubleComplex(sqrtf(r) * cosf(theta / 2.0f),
                                                 //sqrtf(r) * sinf(theta / 2.0f));
 	return sqrt_z;
 }*/
 
-void bChol(float2* A,int i,int N){
-	float complex z = A[i*N+i].x + A[i*N+i].y*I;
+void bChol(double2* A,int i,int N){
+	double complex z = A[i*N+i].x + A[i*N+i].y*I;
 	z = csqrtf(z);
-	float2 a = {.x = creal(z),.y = cimag(z)};
+	double2 a = {.x = creal(z),.y = cimag(z)};
 	A[i*N+i] = a;
 }
 
-void bChol2(float2* A,int i,int N){
+void bChol2(double2* A,int i,int N){
 	//int row = blockIdx.x + 1;
 	//int diagonal = i*N+i;
 	/*if(row == 1){
@@ -154,12 +154,12 @@ void bChol2(float2* A,int i,int N){
 	//printf("\n %d %d",i*N+i, (i*N+i)+row);
 	int range = N-i-1;//N-(i+1);
 	for(int row = 0; row<range; row++){
-		float2 z = {.x = (A[(i*N+i)+row+1].x * A[i*N+i].x + A[(i*N+i)+row+1].y * A[i*N+i].y)/(A[i*N+i].x*A[i*N+i].x + A[i*N+i].y*A[i*N+i].y), .y = (A[(i*N+i)+row+1].y * A[i*N+i].x  - A[(i*N+i)+row+1].x*A[i*N+i].y)/(A[i*N+i].x*A[i*N+i].x + A[i*N+i].y*A[i*N+i].y)};//cuCdivf(A[(i*N+i)+row], A[i*N+i]);
+		double2 z = {.x = (A[(i*N+i)+row+1].x * A[i*N+i].x + A[(i*N+i)+row+1].y * A[i*N+i].y)/(A[i*N+i].x*A[i*N+i].x + A[i*N+i].y*A[i*N+i].y), .y = (A[(i*N+i)+row+1].y * A[i*N+i].x  - A[(i*N+i)+row+1].x*A[i*N+i].y)/(A[i*N+i].x*A[i*N+i].x + A[i*N+i].y*A[i*N+i].y)};//cuCdivf(A[(i*N+i)+row], A[i*N+i]);
 		A[(i*N+i)+row+1] = z;//(A[(i*N+i)+row].x*A[i*N+i].x + A[(i*N+i)+row].y * A[i*N+i].y)/(A[i*N+i].x*A[i*N+i].x + A[i*N+i].y*A[i*N+i].y);//cuCdivf(A[(i*N+i)+row], A[i*N+i]);
 	}
 }
 
-void bChol3(float2* A, int i, int N){
+void bChol3(double2* A, int i, int N){
 	int j = i+1;
 	int range = N-(i+1);
 	//int row = threadIdx.x + blockDim.x * blockIdx.x; //find what col and row this thread is responsible for
@@ -170,10 +170,10 @@ void bChol3(float2* A, int i, int N){
 			if(row >= col){
 				//printf("vec1 %d vec2 %d (%d,%d) index %d \n",(N*i+i+1)+row,(N*i+i+1)+col,row,col,(col+j)*N+j+row);
 				//printf("%d = %d-%d*%d \n",(col+j)*N+j+row,(col+j)*N+j+row,(N*i+i+1)+row,(N*i+i+1)+col);
-				float2 tmp = A[(N*i+i+1)+col];
+				double2 tmp = A[(N*i+i+1)+col];
 				tmp.y = -tmp.y;
-				float2 mult = {.x=(A[(N*i+i+1)+row].x*tmp.x - A[(N*i+i+1)+row].y*tmp.y), .y=(A[(N*i+i+1)+row].x*tmp.y + A[(N*i+i+1)+row].y*tmp.x)};
-				float2 sub = {.x=(A[(col+j)*N+j+row].x-mult.x),.y=A[(col+j)*N+j+row].y-mult.y};
+				double2 mult = {.x=(A[(N*i+i+1)+row].x*tmp.x - A[(N*i+i+1)+row].y*tmp.y), .y=(A[(N*i+i+1)+row].x*tmp.y + A[(N*i+i+1)+row].y*tmp.x)};
+				double2 sub = {.x=(A[(col+j)*N+j+row].x-mult.x),.y=A[(col+j)*N+j+row].y-mult.y};
 				A[(col+j)*N+j+row] = sub;//(double2){.x=A[(col+j)*N+j+row].x - A[(N*i+i+1)+row].x * A[(col+j)*N+j+row].y, .y=A[(col+j)*N+j+row].y - A[(N*i+i+1)+row].x * A[(col+j)*N+j+row].x - A[(N*i+i+1)+row].y * A[(col+j)*N+j+row].y};
 				//A[(col+j)*N+j+row] = cuCsubf(A[(col+j)*N+j+row],cuCmulf(A[(N*i+i+1)+row],tmp));
 			}
@@ -189,14 +189,14 @@ int main() {
 	//read the Y.csv
 	int num_rows = 128;
 	int num_cols = 1;
-	float2 *Y;
+	double2 *Y;
 	char file[] = "128x8/Y";
 	Y = read_matrix_from_csv(file, num_rows, num_cols);
 
 	//read H.csv
 	num_rows = 128;
 	num_cols = 8;
-	float2 *H;
+	double2 *H;
 	strcpy(file, "128x8/H");
 	H = read_matrix_from_csv(file, num_rows, num_cols);
 
@@ -214,21 +214,21 @@ int main() {
 	printf("-------------------------------------------\n");
 
 /*
-	float2 inv[K*K];
-	float2 HHY[N];
+	double2 inv[K*K];
+	double2 HHY[N];
 	*/
-	float2 *HH, *mHH, *dInv, *dInvH,*dInvM,*dY,*HHY,*dx;
-	HH = (float2 *) malloc(K * N * sizeof(float2));
-	mHH = (float2 *) malloc(K * K * sizeof(float2));
-	HHY = (float2 *) malloc(N * 1 * sizeof(float2));
-	//mHH = (float2 *) malloc(K * K * sizeof(float2));
+	double2 *HH, *mHH, *dInv, *dInvH,*dInvM,*dY,*HHY,*dx;
+	HH = (double2 *) malloc(K * N * sizeof(double2));
+	mHH = (double2 *) malloc(K * K * sizeof(double2));
+	HHY = (double2 *) malloc(N * 1 * sizeof(double2));
+	//mHH = (double2 *) malloc(K * K * sizeof(double2));
 	
 	hermitian_transpose(H, HH, K, N);
 	
 	printf("hermitian transpose------------------------------\n");
 	for(int i=0; i<K; i++){//rows
 		for(int j = 0; j<N; j++){//cols
-			printf("%.9f+%.9fi	",HH[j*K + i].x,HH[j*K + i].y);//K*N
+			printf("%f+%fi	",HH[j*K + i].x,HH[j*K + i].y);//K*N
 		}
 		printf(" ; \n");
 	}
@@ -239,7 +239,7 @@ int main() {
 	printf("matmul------------------------------------------\n");
 	for(int i=0; i<K; i++){//rows
 		for(int j = 0; j<K; j++){//cols
-			printf("%.9f+%.9fi	",mHH[j*K + i].x,mHH[j*K + i].y);//K*K
+			printf("%f+%fi	",mHH[j*K + i].x,mHH[j*K + i].y);//K*K
 		}
 		printf(" ; \n");
 	}
@@ -258,9 +258,28 @@ int main() {
 	
 	for(int i = 0; i < K; i++){
 		//Del1 of cholesky. (Diagonal element) one thread
-		bChol(mHH,i,K);	
+		bChol(mHH,i,K);
+		
+		printf("chol part 1------------------------------------------\n");
+		for(int i=0; i<K; i++){//rows
+			for(int j = 0; j<K; j++){//cols
+				printf("%f+%fi	",mHH[j*K + i].x,mHH[j*K + i].y);//K*1
+			}
+			printf(" ; \n");
+		}
+		printf("------------------------------------------\n");		
+			
 		bChol2(mHH,i,K);
 		
+				printf("chol part 2------------------------------------------\n");
+		for(int i=0; i<K; i++){//rows
+			for(int j = 0; j<K; j++){//cols
+				printf("%f+%fi	",mHH[j*K + i].x,mHH[j*K + i].y);//K*1
+			}
+			printf(" ; \n");
+		}
+		printf("------------------------------------------\n");		
+			
 		//bChol<<<1,1>>>(mHH,i,N);
 		//cudaDeviceSynchronize();
 		//Part2 of cholesky (column compleeted)
@@ -274,6 +293,16 @@ int main() {
 	//	blockDims.y = N-(i+1);
 		//bChol3<<<blockDims,1>>>(dmHH,i,N);
 		bChol3(mHH,i,K);
+		
+				printf("chol part 3------------------------------------------\n");
+		for(int i=0; i<K; i++){//rows
+			for(int j = 0; j<K; j++){//cols
+				printf("%f+%fi	",mHH[j*K + i].x,mHH[j*K + i].y);//K*1
+			}
+			printf(" ; \n");
+		}
+		printf("------------------------------------------\n");		
+			
 		//cudaDeviceSynchronize();
 		//Part2 of inv
 		//blockDims.x = N-(i+1);
