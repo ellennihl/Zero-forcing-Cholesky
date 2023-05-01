4ef86e09aecb5fd37574e29ffb1657b1762a5d2b
Ellen <50663696+ellennihl@users.noreply.github.com>
2023-05-01 18:17:27 +0200

    added best avg times

--- matlab_plots/plots_grid_block.m
+++ matlab_plots/plots_grid_block.m
@@ -1,18 +1,19 @@
 
 %[size,time] = readvars("1024x64_runs.txt")
-[size,time] = readvars("4096x128_runs.txt")
+[size,time] = readvars("top_times.txt")
 
 %time = time*1000;
 
 %newStr = extractAfter(size,5)
-newStr = extractAfter(size,8)
-size = categorical(newStr)
+%newStr = extractAfter(size,8)
+%size = categorical(newStr)
+size = categorical(size)
 size = reordercats(size,string(size));
 %newStr = substr(size,4,length)
 
 bar(size, time);
 %title('1024x64 Average execution time') %1024x64 4096x64 1024x128 2048x128
 %4096x128
-title('4096x128 Average execution time')
+title('Best average execution times')
 xlabel('size (block,grid)')
 ylabel('Average execution time [milliseconds]')
