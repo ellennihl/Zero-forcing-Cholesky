16c43107432acdca56f264c343720d7519bfd86b
Ellen <50663696+ellennihl@users.noreply.github.com>
2023-05-01 18:02:23 +0200

    more plots

--- matlab_plots/plots_grid_block.m
+++ matlab_plots/plots_grid_block.m
@@ -1,6 +1,6 @@
 
 %[size,time] = readvars("1024x64_runs.txt")
-[size,time] = readvars("1024x128_runs.txt")
+[size,time] = readvars("4096x128_runs.txt")
 
 %time = time*1000;
 
@@ -11,7 +11,8 @@ size = reordercats(size,string(size));
 %newStr = substr(size,4,length)
 
 bar(size, time);
-%title('1024x64 Average execution time') %1024x64 4096x64 1024x128
-title('1024x128 Average execution time')
+%title('1024x64 Average execution time') %1024x64 4096x64 1024x128 2048x128
+%4096x128
+title('4096x128 Average execution time')
 xlabel('size (block,grid)')
 ylabel('Average execution time [milliseconds]')
